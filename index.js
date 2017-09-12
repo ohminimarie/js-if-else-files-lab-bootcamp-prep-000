37daae86d92cb5df29ad20b05c4a04384567116a
Marie Alcantara <mcjalcantara@gmail.com>
2017-09-12 00:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var height = "74"
 module.exports = { name, height
 }
 
-if (name === "Susan"){
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan.")
-}
+// if (name === "Susan"){
+//   console.log("The name is correct")
+// } else {
+//   console.log("Expected the name to be Susan.")
+// }
